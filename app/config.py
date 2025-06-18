8ff3c31f0dcb954e121a9291a8480115d3eba619
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-06-18 22:34:22 +0800

    update config, dependencies, redis

---
+++ app/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings
+from typing import List, Optional
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    #App settings
+    app_name: str = "RingkasAI"
+    app_version: str = "1.0.0"
+    debug: bool = True
+
+    # CORS settings
+    allowed_origins: List[str] = [
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173"
+    ]
+
+    # Azure OpenAI setting
+    azure_openai_api_key: str
+    azure_openai_api_version: str
+    azure_openai_endpoint: str
+    azure_openai_deployment_name: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = False
+    
+settings = Settings()
