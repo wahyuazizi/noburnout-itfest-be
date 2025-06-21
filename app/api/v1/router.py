7176e3ff79b69c27035c98cd71abcc2eee38fe71
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-06-21 10:15:06 +0800

    add router

---
+++ app/api/v1/router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from app.api.v1.endpoints import transcript, download
+
+api_router = APIRouter()
+
+api_router.include_router(
+    transcript.router,
+    prefix="/transcript",
+    tags=["transcript"]
+)
+
+api_router.include_router(
+    download.router,
+    prefix="/download",
+    tags=["download"]
+)
